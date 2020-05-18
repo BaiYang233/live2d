b71e2fa5c2b6f1fde84535ddb120b59f036fa99d
BaiYang233 <15171328446@163.com>
2020-05-18 23:56:31 +0800

    live2d

--- live2d_load_own/autoload.js
+++ live2d_load_own/autoload.js
@@ -1,6 +1,6 @@
 // 注意：live2d_path 参数应使用绝对路径，记得修改哦
-const live2d_path = "https://cdn.jsdelivr.net/gh/BaiYang233/live2d@master/live2d_load_own/";
-const tips_path = "https://cdn.jsdelivr.net/gh/BaiYang233/live2d@master/live2d_load_own/";
+const live2d_path = "https://cdn.jsdelivr.net/gh/BaiYang233/live2d@1.0.3/live2d_load_own/";
+const tips_path = "https://cdn.jsdelivr.net/gh/BaiYang233/live2d@1.0.3/live2d_load_own/";
 //const live2d_path = "";
 //const live2d_path = "/live2d-widget/";
 
@@ -34,8 +34,8 @@ function loadExternalResource(url, type) {
 	]).then(() => {
 		initWidget({
 			waifuPath: tips_path + "waifu-tips.min.json",
-			apiPath: "https://cdn.jsdelivr.net/gh/BaiYang233/live2d@master/live2d_api/",
-			cdnPath: "https://cdn.jsdelivr.net/gh/BaiYang233/live2d@master/live2d_api/"
+			apiPath: "https://cdn.jsdelivr.net/gh/BaiYang233/live2d@1.0.3/live2d_api/",
+			cdnPath: "https://cdn.jsdelivr.net/gh/BaiYang233/live2d@1.0.3/live2d_api/"
 		});
 	});
 console.log(`
