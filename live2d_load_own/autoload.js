1552269debbbe8937687accbc7655e88a8707556
BaiYang233 <1904261458@qq.com>
2021-07-23 15:13:32 +0800

    live2d 1.0.6

--- live2d_load_own/autoload.js
+++ live2d_load_own/autoload.js
@@ -1,6 +1,6 @@
 // 注意：live2d_path 参数应使用绝对路径，记得修改哦
-const live2d_path = "https://cdn.jsdelivr.net/gh/BaiYang233/live2d@1.0.5/live2d_load_own/";
-const tips_path = "https://cdn.jsdelivr.net/gh/BaiYang233/live2d@1.0.5/live2d_load_own/";
+const live2d_path = "https://cdn.jsdelivr.net/gh/BaiYang233/live2d@1.0.6/live2d_load_own/";
+const tips_path = "https://cdn.jsdelivr.net/gh/BaiYang233/live2d@1.0.6/live2d_load_own/";
 //const live2d_path = "";
 //const live2d_path = "/live2d-widget/";
 
